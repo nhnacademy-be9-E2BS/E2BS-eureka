71c3bf4ea5527580bed891a8ae6d081870025776
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-19 11:11:13 +0900

    modify(SecurityConfig): 모든 경로 인증 허용으로 수정

--- src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
@@ -16,8 +16,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable);
 		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-			.requestMatchers("/actuator/health").permitAll()
-			.anyRequest().authenticated()
+			.anyRequest().permitAll()
 		);
 
 		http.httpBasic(Customizer.withDefaults());
