fcb209529b76ccc51d42a789b15dc734007929cf
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-27 20:11:01 +0900

    hotfix

--- src/main/java/com/nhnacademy/eureka/EurekaApplication.java
+++ src/main/java/com/nhnacademy/eureka/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer // 서비스 디스커버리 서버 (Eureka) 역할을 활성화 
+@EnableEurekaServer // 서비스 디스커버리 서버 (Eureka) 역할을 활성화  
 @SpringBootApplication
 public class EurekaApplication {
 
