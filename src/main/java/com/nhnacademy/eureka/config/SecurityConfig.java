accc53eaa058f7e1d7ffdb6318ecdf35161e2ae7
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-30 17:52:48 +0900

    modify(SecurityConfig): crsf able로 수정

--- src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -13,7 +14,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+		http.csrf(AbstractHttpConfigurer::disable);
 		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
 			.anyRequest().permitAll()
 		);
