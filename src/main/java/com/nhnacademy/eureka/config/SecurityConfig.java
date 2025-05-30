34114b66982f220facc25ea7d7f1001f0ef8825c
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-05-30 15:55:38 +0900

    modify(SecurityConfig): crsf able로 수정

--- src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/eureka/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable);
+
 		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
 			.anyRequest().permitAll()
 		);
